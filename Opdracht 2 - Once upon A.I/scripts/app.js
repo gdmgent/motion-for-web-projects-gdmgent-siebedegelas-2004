c4785d44985175d0131f1563e05867bfa45987de
gdmgent-siebedegelas <siebe.degelas@student.arteveldehs.be>
2023-04-23 19:43:26 +0200

    desktop>>>laptop

--- Opdracht 2 - Once upon A.I/scripts/app.js
+++ Opdracht 2 - Once upon A.I/scripts/app.js
@@ -1,8 +1,24 @@
 console.log("app.js loaded");
 
-let timeline = gsap.timeline({repeat: 0, yoyo: true});
+gsap.registerPlugin(ScrollTrigger);
 
-timeline.to("h1", {duration:1, x: 300, y:200, rotation:360, backgroundColor: "purple"});
-timeline.to(".box", {duration:1, x: 300, y:200, rotation:360, backgroundColor: "red", ease: "bounce"});
+gsap.to(".square", {
+    x: 800,
+    y: 200,
+    duration: 6,
+    // scrollTrigger: ".square"
+})
 
-timeline.from("li", {duration: 0.2, x: "-300px", opacity: 0 , stagger: 0.5});
+
+
+
+
+// let timeline = gsap.timeline({repeat: 0, yoyo: true});
+
+// timeline.to("h1", {duration:1, x: 300, y:200, rotation:360, backgroundColor: "purple"});
+// timeline.to(".box", {duration:1, x: 300, y:200, rotation:360, backgroundColor: "red", ease: "bounce"});
+
+// timeline.from("li", {duration: 0.2, x: "-300px", opacity: 0 , stagger: 0.5});
+
+
+
