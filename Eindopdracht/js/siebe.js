252e34748a77fedb41dee4722aac7a7a99e04487
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2023-05-31 14:05:36 +0200

    Updates

--- Eindopdracht/js/siebe.js
+++ Eindopdracht/js/siebe.js
@@ -16,33 +16,37 @@ function siebe(fase) {
          */
 // Add 1
             .add('start')
-            .from("#siebe p:first-of-type", {
-                opacity: 0,
-                x: -300,
-                y: 300,
-            }, 'start')
-            .from("#siebe p:last-of-type", {
-                y: 300,
-            }, 'start')
-            .from("#siebe #measurments", {
-                opacity: 0,
-                x: -20000,
-            }, 'start')
-            .to("#siebe .box", {
-                borderRadius: "50%",
-                backgroundColor: "#404E4D",
-                rotation: 360,
-                left: '30vw',
-                top: 'calc(50vh - 20vw)',
-                width: '40vw',
-                height: '40vw',
-            //     onComplete: function () {
-            //       var image = document.createElement('img');
-            //       image.src = './images/cirkel.png';
-            //       image.classList.add('cirkel-image');
-            //       document.querySelector('#siebe .box').appendChild(image);
-            //     },
-              }, 'start+=0.5')
+                .to("#siebe .time", {
+                    backgroundColor: "red"
+                }, "start")
+                .from("#siebe p:first-of-type", {
+                    opacity: 0,
+                    x: -300,
+                    y: 300,
+                }, 'start')
+                .from("#siebe p:last-of-type", {
+                    y: 300,
+                }, 'start')
+                .from("#siebe #measurments", {
+                    opacity: 0,
+                    x: -20000,
+                }, 'start')
+                .to("#siebe .box", {
+                    borderRadius: "50%",
+                    backgroundColor: "#404E4D",
+                    rotation: 360,
+                    left: '30vw',
+                    top: 'calc(50vh - 20vw)',
+                    width: '40vw',
+                    height: '40vw',
+                //     onComplete: function () {
+                //       var image = document.createElement('img');
+                //       image.src = './images/cirkel.png';
+                //       image.classList.add('cirkel-image');
+                //       document.querySelector('#siebe .box').appendChild(image);
+                //     },
+                }, 'start')
+               
 
 
          /*
@@ -50,34 +54,26 @@ function siebe(fase) {
          */
 // Add 2
             .add('cirkel1')
-            .to("#siebe .box", {
-                borderRadius: "50%" ,
-                backgroundColor: "#63595C",
-                rotation: 180,
-                left: '30vw',
-                top: 'calc(50vh - 20vw)',
-                width: '40vw',
-                height: '40vw',
-            }, 'cirkel1')
-            .to("#siebe #measurments", {
-                opacity: 1,
-            }, 'cirkel1+=0.5')
-            .to("#siebe .box", {
-                borderRadius: "180"
-            }, 'cirkel1')
+                .to("#siebe .time", {
+                    backgroundColor: "green"
+                }, "cirkel1")
+                .to("#siebe .box", {
+                    borderRadius: "50%" ,
+                    backgroundColor: "#63595C",
+                    rotation: 180,
+                    left: '30vw',
+                    top: 'calc(50vh - 20vw)',
+                    width: '40vw',
+                    height: '40vw',
+                }, 'cirkel1')
+                .to("#siebe #measurments", {
+                    opacity: 1,
+                }, 'cirkel1')
+                .to("#siebe .box", {
+                    borderRadius: "180"
+                }, 'cirkel1')
 
-            .to("#siebe .box", {
-                backgroundColor: "#7d98a1",
-                width: '20vw',
-                height: '20vw',
-                borderRadius: "0",
-                rotateX: 45,
-                rotateY: 45,
-                rotateZ: 45,
-            }, 'reset-img')
-            .to("#siebe #measurments", {
-                opacity: 0,
-            }, 'reset-img+=0.5')
+               
             // .to(".cirkel-image", {
             //     opacity: 0,
             //         onComplete: function () {
@@ -86,215 +82,233 @@ function siebe(fase) {
             //             image.parentNode.removeChild(image);
             //         }
             //     },
-            // }, 'cirkel1+=0.5')
+            // }, 'cirkel1')
          /*
          ** Reset image
          */
 // Add 3
             .add('reset-img')
-            .to("#siebe .box", {
-                backgroundColor: "red",
-                width: '20vw',
-                height: '20vw',
-                borderRadius: "0",
-                rotateX: 45,
-                rotateY: 45,
-                rotateZ: 50,
-                x: '40vw',
-            }, 'reset-img')
+                .to("#siebe #measurments", {
+                    opacity: 0,
+                }, 'reset-img')
+                .to("#siebe .time", {
+                    backgroundColor: "blue"
+                }, "reset-img")
+                .to("#siebe .box", {
+                    backgroundColor: "red",
+                    width: '20vw',
+                    height: '20vw',
+                    borderRadius: "0",
+                    rotateX: 45,
+                    rotateY: 45,
+                    rotateZ: 50,
+                    x: '40vw',
+                }, 'reset-img')
          /*
          ** Diamond poker
          */
 // Add 4
             .add('diamond')
-            .to("#siebe .info-poker", {
-                opacity: 0,
-                x: -500,
-            }, 'diamond')
-            .to("#siebe #harten", {
-                opacity: 1,
-                y: -600,
-                x: 200,
-                rotateZ: -5,
-                ease: "power2.easeIn"
-            }, "diamond")
-            .to("#siebe #schoppen", {
-                opacity: 1,
-                y: -400,
-                x: 500,
-                rotateZ: 5,
-                ease: "power2.easeIn"
-            }, "diamond+=0.2")
-            .to("#siebe #klavers", {
-                opacity: 1,
-                y: -900,
-                x: 800,
-                rotateZ: 5,
-                ease: "power2.easeIn"
-            }, "diamond+=0.4")
-            .to("#siebe #box", {
-                opacity: 0,
-                y: -500,
-                x: -400,
-                ease: "power2.easeIn"
-            }, "diamond")
+                .to("#siebe .time", {
+                    backgroundColor: "yellow"
+                }, "diamond")
+                .to("#siebe .info-poker", {
+                    opacity: 0,
+                    x: -500,
+                }, 'diamond')
+                .to("#siebe #harten", {
+                    opacity: 1,
+                    y: -600,
+                    x: 200,
+                    rotateZ: -5,
+                    ease: "power2.easeIn"
+                }, "diamond")
+                .to("#siebe #schoppen", {
+                    opacity: 1,
+                    y: -400,
+                    x: 500,
+                    rotateZ: 5,
+                    ease: "power2.easeIn"
+                }, "diamond")
+                .to("#siebe #klavers", {
+                    opacity: 1,
+                    y: -900,
+                    x: 800,
+                    rotateZ: 5,
+                    ease: "power2.easeIn"
+                }, "diamond")
+                .to("#siebe #box", {
+                    opacity: 0,
+                    y: -500,
+                    x: -400,
+                    ease: "power2.easeIn"
+                }, "diamond")
          /*
          ** Cards
          */
 // Add 5
             .add('cards')
-            .to("#siebe .cards", {
-                opacity: 1,
-            }, 'cards')
-            .to("#siebe .koningin", {
-                opacity: 1,
-                y: -300,
-            }, 'cards')
-            .to("#siebe .box", {
-                backgroundColor: "#ff5555",
-                width: '10vw',
-                height: '10vw',
-                borderRadius: "5%",
-                rotateX: 45,
-                rotateY: 45,
-                rotateZ: 55,
-                x: '10vw',
-                y: '10vw',
-            }, 'cards')
+                .to("#siebe .cards", {
+                    opacity: 1,
+                }, 'cards')
+                .to("#siebe .koningin", {
+                    opacity: 1,
+                    y: -300,
+                }, 'cards')
+                .to("#siebe .box", {
+                    backgroundColor: "#ff5555",
+                    width: '10vw',
+                    height: '10vw',
+                    borderRadius: "5%",
+                    rotateX: 45,
+                    rotateY: 45,
+                    rotateZ: 55,
+                    x: '10vw',
+                    y: '10vw',
+                }, 'cards')
          /*
          ** Chill
          */
 // Add 6
             .add('chill')
-            .to("#siebe .chill", {
-                opacity: 0,
-            }, 'chill')
+                .to("#siebe .chill", {
+                    opacity: 0,
+                }, 'chill')
+                .to("#siebe .box", {
+                    display: "none",
+                }, 'chill')
+                /*.to("#siebe .koningin", { 
+                    y: +400,
+                })*/
+                
          /*
          ** Cirkel 2
          */
 // Add 7
             .add('cirkel2')
-            .to("#siebe .box", {
-                backgroundColor: "#7d98a1",
-                width: '20vw',
-                height: '20vw',
-                borderRadius: "0",
-                rotateX: 45,
-                rotateY: 45,
-                rotateZ: 45,
-                x: '10vw',
-            }, 'cirkel2')
-            .to("#siebe .box", {
-                backgroundColor: "#7d98a1",
-            }, 'cirkel2')
-            .to("#siebe .box", {
-                backgroundColor: "#2b3628",
-                width: '40vw',
-                height: '40vw',
-                borderRadius: "0",
-                rotateX: 180,
-                rotateY: 180,
-                rotateZ: 180,
-                y: '10vw',
-                opacity: 0,
-                backgroundColor: "#2b3628",
-                width: '5vw',
-                height: '5vw',
-            }, 'cirkel2')
-            .from("#siebe p:first-of-type", {
-                opacity: 0,
-                x: -300,
-            }, 'cirkel2')
+                
+                /*.to("#siebe .box", {
+                    backgroundColor: "#7d98a1",
+                    width: '20vw',
+                    height: '20vw',
+                    borderRadius: "0",
+                    rotateX: 45,
+                    rotateY: 45,
+                    rotateZ: 45,
+                    x: '10vw',
+                }, 'cirkel2')
+     
+                .to("#siebe .box", {
+                    backgroundColor: "#2b3628",
+                    width: '40vw',
+                    height: '40vw',
+                    borderRadius: "0",
+                    rotateX: 180,
+                    rotateY: 180,
+                    rotateZ: 180,
+                    y: '10vw',
+                    opacity: 0,
+                    backgroundColor: "#2b3628",
+                    width: '5vw',
+                    height: '5vw',
+                }, 'cirkel2')
+                */
+                .from("#siebe p:first-of-type", {
+                    opacity: 0,
+                    x: -300,
+                }, 'cirkel2')
          /*
          ** Illuminati
          */
 // Add 8
             .add('illuminati')
-            .from("#text-illuminati", {
-                x: -400,
-            }, 'illumnati')
-            .to("#siebe #illuminati-img", {
-                opacity: 1,
-                rotateZ: 5,
-                y: -200,
-                ease: "power2.easeIn"
-            }, "illuminati-img")
-            .to ("#siebe .box", {
-                backgroundColor: "#2b3628",
-                width: '15vw',
-                height: '15vw',
-                borderRadius: "180",
-                opacity: 0,
-            } , 'box')
-            .to("#siebe .cards", {
-                opacity: 0,
-                y: -1000,
-            }, 'illuminati')
+                .from("#text-illuminati", {
+                    x: -400,
+                }, 'illumnati')
+                .to("#siebe #illuminati-img", {
+                    opacity: 1,
+                    rotateZ: 5,
+                    y: -200,
+                    ease: "power2.easeIn"
+                }, "illuminati-img")
+                .to ("#siebe .box", {
+                    backgroundColor: "#2b3628",
+                    width: '15vw',
+                    height: '15vw',
+                    borderRadius: "180",
+                    opacity: 0,
+                } , 'box')
+                .to("#siebe .cards", {
+                    opacity: 0,
+                    y: -1000,
+                }, 'illuminati')
         /*
          ** Pizza
          */
 // Add 9
          .add('pizza')
-         .to("#siebe .box", {
-             backgroundColor: "#fee194",
-             width: '20vw',
-             height: '20vw',
-             borderRadius: "180",
-             border: '20px solid #bc6201',
-             opacity: 1,
-         })
-         .to("#siebe .box", {
-            backgroundColor: "#fee194",
-            width: '20vw',
-            height: '20vw',
-            borderRadius: "180",
-            border: '20px solid #bc6201',
-        }, 'pizza')
-        .to("#siebe #honger", {
-            x: -1000,
-        }, 'pizza')
+            .to("#siebe .box", {
+                backgroundColor: "#fee194",
+                width: '20vw',
+                height: '20vw',
+                borderRadius: "180",
+                border: '20px solid #bc6201',
+                opacity: 1,
+            })
+            .to("#siebe .box", {
+                backgroundColor: "#fee194",
+                width: '20vw',
+                height: '20vw',
+                borderRadius: "180",
+                border: '20px solid #bc6201',
+            }, 'pizza')
+            .to("#siebe #honger", {
+                x: -1000,
+            }, 'pizza')
         // Sauce
 // Add 10
+/*
         .add('tSauce')
-        .to("#siebe .sauce", {
-            opacity: 1,
-            ease: "power2.easeIn",
-        }, "tSauce")
-        .to("#siebe .cheese", {
-            opacity: 1,
-            ease: "power2.easeIn",
-        }, "tSauce")
-        .to("#siebe .pepperoni", {
-            opacity: 1,
-            ease: "power2.easeIn",
-        }, "tSauce")
-        .to("#siebe .box", {
-            backgroundColor: "#fee194",
-            width: '20vw',
-            height: '20vw',
-            borderRadius: "180",
-            border: '20px solid #bc6201',
-        }, 'tSauce')
+            .to("#siebe .sauce", {
+                opacity: 1,
+                ease: "power2.easeIn",
+            }, "tSauce")
+            .to("#siebe .cheese", {
+                opacity: 1,
+                ease: "power2.easeIn",
+            }, "tSauce")
+            .to("#siebe .pepperoni", {
+                opacity: 1,
+                ease: "power2.easeIn",
+            }, "tSauce")
+            .to("#siebe .box", {
+                backgroundColor: "#fee194",
+                width: '20vw',
+                height: '20vw',
+                borderRadius: "180",
+                border: '20px solid #bc6201',
+            }, 'tSauce')
+            */
 
         /*
          ** Reset
          */
 // Add 11
             .add('reset')
-            .to("#siebe .box", {
-                borderRadius: "0" ,
-                backgroundColor: 'white',
-                rotateX: 0,
-                rotateY: 0,
-                rotateZ: 0,
-                width: '20vw',
-                height: '20vw',
-                y: '0vw',
-                x: '0vw',
-                left: '40vw',
-                top: 'calc(50vh - 10vw)',
-                border: 'none',
-            }, 'reset');
+                .to("#siebe .box", {
+                    borderRadius: "0" ,
+                    backgroundColor: 'white',
+                    rotateX: 0,
+                    rotateY: 0,
+                    rotateZ: 0,
+                    width: '20vw',
+                    height: '20vw',
+                    y: '0vw',
+                    x: '0vw',
+                    left: '40vw',
+                    top: 'calc(50vh - 10vw)',
+                    border: 'none',
+                }, 'reset');
 
 
     }
